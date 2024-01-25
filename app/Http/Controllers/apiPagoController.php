fb4020a21a114c7346327f5bad9149f5f847b0ec
Ruben Gadea Simon <rgs103@gcloud.ua.es>
2024-01-25 11:21:31 +0100

    nada importante

--- app/Http/Controllers/apiPagoController.php
+++ app/Http/Controllers/apiPagoController.php
@@ -10,15 +10,14 @@ class apiPagoController extends Controller
     public function llamarAPI()
     {
         // Datos de autenticación y otros datos necesarios
-        $token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTB9.AmfT0rGLx2V1YHI9iBdcPCE6-Y41M6FJ5otw5uJrNd0';  // Reemplaza con tu token JWT
-        $concepto = 'Añadir saldo';  // Reemplaza con el concepto deseado
-        $cantidad = 100;  // Reemplaza con la cantidad deseada
+        $token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTB9.AmfT0rGLx2V1YHI9iBdcPCE6-Y41M6FJ5otw5uJrNd0';
+        $concepto = 'Añadir saldo';
+        $cantidad = 100;
         // Generar un número aleatorio para receipt_number
         $receiptNumber = rand(100000, 999999);
         // URL de la API
         $url = 'https://ebisu.firstrow2.com/api/transactions';
 
-        // Realizar la solicitud POST a la API con los datos requeridos
         $response = Http::withHeaders([
             'Authorization' => 'Bearer ' . $token,
             'Content-Type' => 'application/json',
@@ -26,7 +25,6 @@ class apiPagoController extends Controller
             'concept' => $concepto,
             'amount' => $cantidad,
             'receipt_number' => (string)$receiptNumber,
-            // Agrega otros datos según sea necesario para la API
         ]);
 
         // Obtener el HTML desde la respuesta
@@ -52,8 +50,7 @@ class apiPagoController extends Controller
             'Authorization' => 'Bearer ' . $token,
             'Content-Type' => 'application/json',
         ])->post($url, [
-            // Agrega los datos necesarios para completar la transacción
-            // Puedes necesitar proporcionar información adicional según las especificaciones de la API
+
         ]);
 
         // Obtener la respuesta desde la API después de completar la transacción
